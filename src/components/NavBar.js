a6963ba314f4797b0cb3e0d24b23438848996f28
Chris <cfield435@gmail.com>
2024-09-01 19:21:01 -0600

    added responsive version

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,10 +1,23 @@
 import React from 'react';
 
 function NavBar() {
+
+  document.addEventListener('DOMContentLoaded', function() {
+    const toggleButton = document.querySelector('.menu-toggle');
+    const navList = document.querySelector('.nav-list');
+  
+    toggleButton.addEventListener('click', function() {
+      navList.classList.toggle('open');
+    });
+  });
+
   return (
     <>
       <nav className='navbar'>
         <p className='logo'>logo</p>
+        <button class="menu-toggle" aria-label="Toggle menu">
+          ☰
+        </button>
         <ul className='nav-list'>
           <li>About us</li>
           <li>Services</li>
